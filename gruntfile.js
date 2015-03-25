982d5a0f860f1351dfe32f07edcf14abfc452ecb
katenko <passw0rd>
2015-03-26 08:56:50 +1000

    initial

--- gruntfile.js
+++ gruntfile.js
@@ -1,13 +1,6 @@
 'use strict';
 
 module.exports = function (grunt) {
-    // Unified Watch Object
-    //var watchFiles = {
-    //    serverViews: ['madmin/app/views/**/*.*'],
-    //    serverJS: ['gruntfile.js', 'server.js', 'config/**/*.js', 'madmin/app/**/*.js'],
-    //    mochaTests: ['madmin/app/tests/**/*.js']
-    //};
-
     // Project Configuration
     grunt.initConfig({
         project: {
@@ -236,41 +229,6 @@ module.exports = function (grunt) {
         //        }
         //    }
         //},
-        watch: {
-            file: '**/*.html',
-            options: {
-                livereload: true
-            },
-            styles: {
-                files: ['**/*.less'],
-                tasks: ['less'],
-                options: {
-                    nospawn: true
-                }
-            },
-            static: {
-                files: ['**/*.html'],
-                tasks: ['copy'],
-                options: {
-                    nospawn: true
-                }
-            },
-            //jade: {
-            //    files: ['**/*.jade'],
-            //    tasks: ['jade'],
-            //    options: {
-            //        nospawn: true
-            //    }
-            //},
-            //gruntfile: {
-            //    files: 'Gruntfile.js',
-            //    tasks: ['default']
-            //},
-            javascript: {
-                files: '<%= project.javascript.ours %>',
-                tasks: ['jshint', 'ngtemplates', 'concat']
-            }
-        },
         concat: {
             javascript_ours: {
                 options: {
@@ -372,142 +330,7 @@ module.exports = function (grunt) {
                 ]
             }
         }
-        //watch: {
-        //	serverViews: {
-        //		files: watchFiles.serverViews,
-        //		options: {
-        //			livereload: true
-        //		}
-        //	},
-        //	serverJS: {
-        //		files: watchFiles.serverJS,
-        //		tasks: ['jshint'],
-        //		options: {
-        //			livereload: true
-        //		}
-        //	},
-        //	clientViews: {
-        //		files: watchFiles.clientViews,
-        //		options: {
-        //			livereload: true,
-        //		}
-        //	},
-        //	clientJS: {
-        //		files: watchFiles.clientJS,
-        //		tasks: ['jshint'],
-        //		options: {
-        //			livereload: true
-        //		}
-        //	},
-        //	clientCSS: {
-        //		files: watchFiles.clientCSS,
-        //		tasks: ['csslint'],
-        //		options: {
-        //			livereload: true
-        //		}
-        //	}
-        //},
-        //jshint: {
-        //	all: {
-        //		src: watchFiles.clientJS.concat(watchFiles.serverJS),
-        //		options: {
-        //			jshintrc: true
-        //		}
-        //	}
-        //},
-        //csslint: {
-        //	options: {
-        //		csslintrc: '.csslintrc',
-        //	},
-        //	all: {
-        //		src: watchFiles.clientCSS
-        //	}
-        //},
-        //nodemon: {
-        //	dev: {
-        //		script: 'server.js',
-        //		options: {
-        //			nodeArgs: ['--debug'],
-        //			ext: 'js,html',
-        //			watch: watchFiles.serverViews.concat(watchFiles.serverJS)
-        //		}
-        //	}
-        //},
-        //'node-inspector': {
-        //	custom: {
-        //		options: {
-        //			'web-port': 1337,
-        //			'web-host': 'localhost',
-        //			'debug-port': 5858,
-        //			'save-live-edit': true,
-        //			'no-preload': true,
-        //			'stack-trace-limit': 50,
-        //			'hidden': []
-        //		}
-        //	}
-        //},
-        //concurrent: {
-        //	default: ['nodemon', 'watch'],
-        //	debug: ['nodemon', 'watch', 'node-inspector'],
-        //	options: {
-        //		logConcurrentOutput: true,
-        //		limit: 10
-        //	}
-        //},
-        //env: {
-        //	test: {
-        //		NODE_ENV: 'test'
-        //	},
-        //	secure: {
-        //		NODE_ENV: 'secure'
-        //	}
-        //},
-        //mochaTest: {
-        //	src: watchFiles.mochaTests,
-        //	options: {
-        //		reporter: 'spec',
-        //		require: 'server.js'
-        //	}
-        //},
-        //karma: {
-        //	unit: {
-        //		configFile: 'karma.conf.js'
-        //	}
-        //}
     });
-
-    //// Load NPM tasks
-    //require('load-grunt-tasks')(grunt);
-    //
-    //// Making grunt default to force in order not to break the project.
-    //grunt.option('force', true);
-    //
-    //// A Task for loading the configuration object
-    //grunt.task.registerTask('loadConfig', 'Task that loads the config into a grunt option.', function () {
-    //    var init = require('./config/init')();
-    //    var config = require('./config/config');
-    //
-    //    grunt.config.set('applicationJavaScriptFiles', config.assets.js);
-    //    grunt.config.set('applicationCSSFiles', config.assets.css);
-    //});
-
-    //// Default task(s).
-    //grunt.registerTask('default', ['lint', 'concurrent:default']);
-    //
-    //// Debug task.
-    //grunt.registerTask('debug', ['lint', 'concurrent:debug']);
-    //
-    //// Secure task(s).
-    //grunt.registerTask('secure', ['env:secure', 'lint', 'concurrent:default']);
-    //
-    //// Lint task(s).
-    //grunt.registerTask('lint', ['jshint']);
-    //
-    //// Build task(s).
-    //grunt.registerTask('build', ['lint']);
-    //
-    //// Test task.
-    //grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
