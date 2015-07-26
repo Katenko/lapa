ede856a005fa1a7586341ea323665920ca6fb6b2
katenko@lanitp.ru <katenko@lanitp.ru>
2015-07-26 14:31:36 +1000

    initial

--- madmin/source/js/services/diagramService.js
+++ madmin/source/js/services/diagramService.js
@@ -4,7 +4,8 @@ App.service('DiagramService', ['$http', '$q',
             var deferred = $q.defer();
             var path;
             if (mode > 0) {
-                path = "http://192.168.23.203:8085/cucm-1.0/data/getData/cdr";
+                //path = "http://192.168.23.203:8085/cucm-1.0/data/getData/cdr";
+                path = "http://localhost:8085/cucm-1.0/data/getData/cdr";
                 $http.jsonp(path + "?callback=JSON_CALLBACK").then(function (data) {
                     deferred.resolve(data.data);
                 });
